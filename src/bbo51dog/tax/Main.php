499621b035940fc9f6b9c05dfa1148d582583818
bbo51dog <bbo51@icloud.com>
2019-09-06 22:06:38 +0900

    config

--- src/bbo51dog/tax/Main.php
+++ src/bbo51dog/tax/Main.php
@@ -3,7 +3,11 @@
 namespace bbo51dog\tax;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
 
 class Main extends PluginBase{
-
+    public function onEnable(){
+        $this->saveResource(Text::CONFIG_NAME);
+        $config = new Config($this->getDataFolder().Text::CONFIG_NAME, Config::YAML);
+    }
 }
---
+++ src/bbo51dog/tax/Text.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace bbo51dog\tax;
+
+class Text{
+    
+    /** @var string */
+    public const CONFIG_NAME = "Config.yml";
+}
