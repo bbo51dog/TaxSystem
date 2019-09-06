a5bfb3877ae176c6ad0990c1e237bc1244a9f838
bbo51dog <bbo51@icloud.com>
2019-09-06 22:36:26 +0900

    Create TaxTask class

---
+++ src/bbo51dog/tax/TaxTask.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace bbo51dog\tax\task;
+
+use pocketmine\scheduler\Task;
+
+class TaxTask extends Task{
+
+    public function __construct(){
+
+    }
+
+    public function onRun(int $tick){
+    
+    }
+}
