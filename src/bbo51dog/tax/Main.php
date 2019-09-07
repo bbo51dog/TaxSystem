83e981ca0b7a368c33a1e041c60060378e557c3d
bbo51dog <bbo51@icloud.com>
2019-09-07 22:21:50 +0900

    del Text.yml

--- src/bbo51dog/tax/Main.php
+++ src/bbo51dog/tax/Main.php
@@ -8,9 +8,7 @@ use pocketmine\utils\Config;
 class Main extends PluginBase{
     public function onEnable(){
         $this->saveResource("Config.yml");
-        $this->saveResource("Text.yml");
         $config = new Config($this->getDataFolder()."Config.yml", Config::YAML);
-        $text = new Config($this->getDataFolder()."Text.yml", Config::YAML);
         $config_data = $config->getAll();
         $task = new TaxTask($config_data["amount"], $config_data["min"], $config_data["time"]);
         $this->getScheduler()->scheduleRepeatingTask($task, 1200);
